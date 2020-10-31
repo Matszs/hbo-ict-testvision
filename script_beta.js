84cffa3132ef840e2e315fc712ac0cf6861c52e4
Mats Otten <Matszs@users.noreply.github.com>
2020-10-31 23:50:45 +0100

    Update script_beta.js

--- script_beta.js
+++ script_beta.js
@@ -1,19 +1,19 @@
 $('<div style="position: fixed; z-index: 30001; bottom: 0; left: 0; background: white; padding: 10px;"><select id="hbo-ict-selector"></select><button id="hbo-ict-selector-add">+</button><button id="hbo-ict-selector-delete">-</button></div>').appendTo('body');
 
 var hboIctScriptDataTxt = localStorage.getItem("hbo_ict_groups");
-if(hboIctScriptData == null) {
+if(hboIctScriptDataTxt == null) {
         hboIctScriptDataTxt = JSON.stringify({"groups": {}});
         localStorage.setItem("hbo_ict_groups", hboIctScriptDataTxt);
 }
 
-hboIctScriptData = JSON.parse(hboIctScriptDataTxt);
+var hboIctScriptData = JSON.parse(hboIctScriptDataTxt);
 
 $('#hbo-ict-selector-add').click(function() {
         var groupName = window.prompt("Naam van de groep");
         var studentNumbers = [...window.prompt("kopieer en plak uit DLO: Groepen -> kolom: Leden -> alle tekst (achternaam, voornaam, emailadres, student nummer)").matchAll(/(500[0-9]{5,})/g)].flat();
 
         hboIctScriptData.groups[groupName] = studentNumbers;
-        localStorage.setItem("hbo_ict_groups", hboIctScriptDataTxt);
+        localStorage.setItem("hbo_ict_groups", JSON.stringify(hboIctScriptData));
         
         $('#hbo-ict-selector').append('<option>' + groupName + '</option>');
 });
